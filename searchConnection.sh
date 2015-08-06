e8da95a57c3c0c33d2207a02fdfc05f867faca32
Alex Ives <alex.ives@code42.com>
2015-08-06 18:06:59 -0500

    Updated searchConnection.sh to grep with no case on the list of configurations. Allowing for regex matches.

--- searchConnection.sh
+++ searchConnection.sh
@@ -4,13 +4,11 @@ QUERY="$1"
 echo "<items>"
 connections=$(ls /Library/Application\ Support/Tunnelblick/Users/$USER | sed 's/\.tblk//g')
 
-for connection in $connections; do
-	if [[ -z "${QUERY}" ]] || [[ "${connection}" = "${QUERY}"* ]]; then
-		echo "<item uuid=\"$(uuidgen)\" arg=\"$connection\" valid=\"YES\">"
-		echo "<title>VPN: $connection</title>"
-		echo "<subtitle>Open VPN connection - $connection</subtitle>"; 
-		echo "</item>"
-	fi
+for connection in $(ls /Library/Application\ Support/Tunnelblick/Users/$USER | grep -i "${QUERY}" | sed 's/\.tblk//g'); do
+	echo "<item uuid=\"$(uuidgen)\" arg=\"$connection\" valid=\"YES\">"
+	echo "<title>VPN: $connection</title>"
+	echo "<subtitle>Open VPN connection - $connection</subtitle>"
+	echo "</item>"
 done
 
 echo "</items>"
